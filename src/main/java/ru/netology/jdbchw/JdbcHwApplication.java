f08f7d587f13b99c14026bc9bf3b6e65cd3da83f
najmovich <najmovich@protei.ru>
2024-12-20 11:30:21 +0300

    Minor fix

--- src/main/java/ru/netology/jdbchw/JdbcHwApplication.java
+++ src/main/java/ru/netology/jdbchw/JdbcHwApplication.java
@@ -9,20 +9,8 @@ import java.util.List;
 
 @SpringBootApplication
 public class JdbcHwApplication {
-    /*private final OrderRepository orderRepository;
-
-    public JdbcHwApplication(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(JdbcHwApplication.class, args);
     }
-
-    /*@Override
-    public void run(String... args) {
-        String name = "Alexey";
-        List<String> product = orderRepository.getProductsNames(name);
-        product.forEach(productName -> System.out.printf("Product name: %s\n", productName));
-    }*/
 }
