a37fa2ba33b89920ded72cf869e4a3c829b84af4
najmovich <najmovich@protei.ru>
2024-12-20 08:40:23 +0300

    Initial commit

---
+++ src/main/java/ru/netology/hdbchw/HdbcHwApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.hdbchw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdbcHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdbcHwApplication.class, args);
+    }
+
+}
